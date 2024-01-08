e4d1eb0204b481b2defd0b1a457e40b22de23b1c
Matheus <matheusmiguelds@gmail.com>
2024-01-08 10:02:56 -0300

    Adicionando estudo sobre expressoes e operadores

---
+++ 5-expressoes_cond_e_operadores/index.php
@@ -0,0 +1,32 @@
+<?php 
+    //if
+    $a = 4;
+    if ($a == 5){
+        echo "É cinco";
+    } else if ($a == 7){
+        echo "É sete";
+    } else {
+        echo "Não é cinco e nem sete";
+    }
+    echo '<br/>';
+
+    //switch
+    $w = 5;
+    switch($w) {
+        case 1:
+            echo 'era um';
+            break;
+        case 2:
+            echo 'era dois';
+            break;
+        case 3:
+            echo 'era três';
+            break;
+        default:
+            echo 'Não é nenhum';
+    }
+    echo '<br/>';
+
+    // Operadores
+    // and ou && /  or ou || /  xor / !
+?>
